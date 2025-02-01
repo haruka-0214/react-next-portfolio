70d3540ff50b0554d15c8ff62531cf70c6668aa9
haruka-0214 <ktc24a31e0015@edu.kyoto-tech.ac.jp>
2025-02-01 16:19:57 +0900

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -15,9 +15,9 @@ export default async function Home() {
     <>
     <section className={styles.top}>
       <div>
-        <h1 className={styles.title}>テクノロジーの力で世界を変える</h1>
+        <h1 className={styles.title}>Murakami Haruka</h1>
         <p className={styles.description}>
-          私たちは市場をリードしているグローバルテックカンパニーです。
+          Kyoto college of Design and technology
         </p>
       </div>
       <Image
